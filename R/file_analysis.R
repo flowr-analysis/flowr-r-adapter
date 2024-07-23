d92170555e30adf6ced5511e9b565b3420ab8a2f
Lukas Pietzschmann <lukas.pietzschmann@uni-ulm.de>
2024-07-23 13:43:38 +0200

    Added support for specifying the content for a file-analysis req

--- R/file_analysis.R
+++ R/file_analysis.R
@@ -2,6 +2,7 @@ source("R/connect.R")
 source("R/utils.R")
 
 #' Sends a ["file-analysis"](https://github.com/flowr-analysis/flowr/wiki/Interface#the-analysis-request) request
+#' Note that either `filepath` or `content` must be provided, but never both.
 #'
 #' @param filepath The path to the file (must be visible to the server)
 #' @param id The id of the request
@@ -10,20 +11,36 @@ source("R/utils.R")
 #' @return A list containing the id, filetoken and the response
 #'
 #' @export
-# TODO: support content and filepath
-request_file_analysis <- function(filepath,
+request_file_analysis <- function(filepath = NULL,
+                                  content = NULL,
                                   id = get_new_id(),
                                   filetoken = get_filetoken(),
                                   con = connect()) {
-  filepath <- normalizePath(filepath, mustWork = FALSE)
-  filetoken <- get_filetoken(filepath)
+  if (is.null(filepath) && is.null(content)) {
+    stop("Either filepath or content must be provided")
+  }
+  if (!is.null(filepath) && !is.null(content)) {
+    stop("filepath and content cannot be provided at the same time")
+  }
 
-  request <- fromRJSON('{
-    "type":      "request-file-analysis",
-    "id":        "%s",
-    "filetoken": "%s",
-    "filepath":  "%s"
-  }', id, filetoken, filepath)
+  if (!is.null(filepath)) {
+    filepath <- normalizePath(filepath, mustWork = FALSE)
+    filetoken <- get_filetoken(filepath)
+    request <- fromRJSON('{
+      "type":      "request-file-analysis",
+      "id":        "%s",
+      "filetoken": "%s",
+      "filepath":  "%s"
+    }', id, filetoken, filepath)
+  } else if (!is.null(content)) {
+    request <- fromRJSON('{
+      "type":      "request-file-analysis",
+      "id":        "%s",
+      "filetoken": "%s",
+      "content":   "%s"
+    }', id, filetoken, content)
+  }
+
   res <- send_request(con, request)
 
   if (handle_err_result(res)) {
