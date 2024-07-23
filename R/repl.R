435a7a10191d0df6b5310111026ede9e9e7f8331
Lukas Pietzschmann <lukas.pietzschmann@uni-ulm.de>
2024-07-23 13:29:45 +0200

    Added repl request

---
+++ R/repl.R
@@ -0,0 +1,21 @@
+source("R/connect.R")
+source("R/utils.R")
+
+#' Sends a ["request-repl-execution"](https://github.com/flowr-analysis/flowr/wiki/Interface#the-repl-request) request
+#'
+#' @param expression The expression to be executed by the REPL
+#' @param id The id of the request
+#' @param con The connection to the server
+#' @return A list containing the id and the response
+#'
+#' @export
+request_repl <- function(expression, id = get_new_id(), con = connect()) {
+  request <- fromRJSON('{
+    "type":       "request-repl-execution",
+    "id":         "%s",
+    "expression": "%s",
+  }', id, expression)
+  res <- send_request(con, request)
+
+  return(list(id = id, res = res))
+}
