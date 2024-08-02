df75ecf8a9064b602a16f4580f3cded0000a5c8a
Ellpeck <me@ellpeck.de>
2024-08-02 13:44:23 +0200

    increase the timeout when installing node in tests

--- tests/testthat/test-node.R
+++ tests/testthat/test-node.R
@@ -2,6 +2,7 @@ test_that("find package directory", {
   expect_no_error(get_default_node_base_dir())
 })
 test_that("install node", {
+  options(timeout = 300)
   expect_no_error(install_node("22.5.1"))
 })
 test_that("install flowr", {
