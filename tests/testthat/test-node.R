0e1855e7d1e02ff7c42d6b0e6b010d4249f714ee
Ellpeck <me@ellpeck.de>
2024-08-02 13:32:14 +0200

    added basic tests closes #9

---
+++ tests/testthat/test-node.R
@@ -0,0 +1,9 @@
+test_that("find package directory", {
+  expect_no_error(get_default_node_base_dir())
+})
+test_that("install node", {
+  expect_no_error(install_node("22.5.1"))
+})
+test_that("install flowr", {
+  expect_no_error(install_flowr("2.0.11"))
+})
