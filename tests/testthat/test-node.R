aad2c64547a76385860b05ed0e0cf42ef66aeff4
Ellpeck <me@ellpeck.de>
2024-10-02 17:57:04 +0200

    test whether the flowr installation worked correctly

--- tests/testthat/test-node.R
+++ tests/testthat/test-node.R
@@ -6,5 +6,14 @@ test_that("install node", {
   expect_no_error(install_node("22.5.1"))
 })
 test_that("install flowr", {
-  expect_equal(install_flowr("2.0.11"), 0)
+  flowr_version <- "2.0.11"
+  expect_equal(install_flowr(flowr_version), 0)
+
+  # test if the installation was actually successful
+  pid <- exec_flowr("--server", background = TRUE)
+  on.exit(tools::pskill(pid), add = TRUE, after = FALSE)
+  conn_hello <- connect()
+  on.exit(flowr::disconnect(conn_hello[[1]]), add = TRUE, after = FALSE)
+  response <- jsonlite::fromJSON(conn_hello[[2]])
+  expect_equal(response$versions$flowr, flowr_version)
 })
