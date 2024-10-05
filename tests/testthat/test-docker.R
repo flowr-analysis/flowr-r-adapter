c576be0d0fa005e83aa682d4eb06d6f35103b295
Ellpeck <me@ellpeck.de>
2024-10-05 16:36:56 +0200

    skip docker tests on windows and mac

--- tests/testthat/test-docker.R
+++ tests/testthat/test-docker.R
@@ -1,3 +1,6 @@
 test_that("run flowr (docker)", {
+  skip_on_os("windows")
+  skip_on_os("mac")
+
   expect_output(exec_flowr_docker(c(), "2.0.11", c("--version"), TRUE), paste0("flowR: ", "2.0.11"))
 })
